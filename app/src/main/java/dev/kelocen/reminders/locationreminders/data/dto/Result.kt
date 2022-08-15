b9bd0b3692dc22599aff136205c161823f87aba5
kelocen <70181196+kelocen@users.noreply.github.com>
2022-08-14 22:26:22 -0500

    Initial Commit

---
+++ app/src/main/java/dev/kelocen/reminders/locationreminders/data/dto/Result.kt
@@ -0,0 +1,24 @@
+package dev.kelocen.reminders.locationreminders.data.dto
+
+
+/**
+ * A sealed class that encapsulates successful outcome with a value of type [T]
+ * or a failure with message and statusCode
+ */
+sealed class Result<out T : Any> {
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val message: String?, val statusCode: Int? = null) :
+        Result<Nothing>()
+
+    /**
+     * Returns true if the [Result] is of type [Success].
+     */
+    val succeeded
+        get() = this is Success
+
+    /**
+     * Returns true if the [Result] is of type [Error].
+     */
+    val error
+        get() = this is Error
+}
---
+++ app/src/main/java/dev/kelocen/reminders/locationreminders/reminderslist/RemindersListFragment.kt
@@ -0,0 +1,86 @@
+package dev.kelocen.reminders.locationreminders.reminderslist
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import dev.kelocen.reminders.R
+import dev.kelocen.reminders.base.BaseFragment
+import dev.kelocen.reminders.base.NavigationCommand
+import dev.kelocen.reminders.databinding.FragmentRemindersListBinding
+import dev.kelocen.reminders.utils.setDisplayHomeAsUpEnabled
+import dev.kelocen.reminders.utils.setTitle
+import dev.kelocen.reminders.utils.setup
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class ReminderListFragment : BaseFragment() {
+    //use Koin to retrieve the ViewModel instance
+    override val viewModel: RemindersListViewModel by viewModel()
+    private lateinit var binding: FragmentRemindersListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_reminders_list, container, false
+            )
+        binding.viewModel = viewModel
+
+        setHasOptionsMenu(true)
+        setDisplayHomeAsUpEnabled(false)
+        setTitle(getString(R.string.app_name))
+
+        binding.refreshLayout.setOnRefreshListener { viewModel.loadReminders() }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+        setupRecyclerView()
+        binding.addReminderFAB.setOnClickListener {
+            navigateToAddReminder()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //load the reminders list on the ui
+        viewModel.loadReminders()
+    }
+
+    private fun navigateToAddReminder() {
+        //use the navigationCommand live data to navigate between the fragments
+        viewModel.navigationCommand.postValue(
+            NavigationCommand.To(
+                ReminderListFragmentDirections.toSaveReminder()
+            )
+        )
+    }
+
+    private fun setupRecyclerView() {
+        val adapter = RemindersListAdapter {
+        }
+
+//        setup the recycler view using the extension function
+        binding.remindersRecyclerView.setup(adapter)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+//                TODO: add the logout implementation
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+//        display logout as menu item
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+}
