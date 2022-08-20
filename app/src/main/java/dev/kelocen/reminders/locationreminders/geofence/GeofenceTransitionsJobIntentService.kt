12f064a2056ed888dbcd424d344b36457c0944f8
kelocen <70181196+kelocen@users.noreply.github.com>
2022-08-19 19:13:50 -0500

    feat: add geofence implementation

--- app/src/main/java/dev/kelocen/reminders/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/dev/kelocen/reminders/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+import dev.kelocen.reminders.R
 import dev.kelocen.reminders.locationreminders.data.ReminderDataSource
 import dev.kelocen.reminders.locationreminders.data.dto.ReminderDTO
 import dev.kelocen.reminders.locationreminders.data.dto.Result
 import dev.kelocen.reminders.locationreminders.reminderslist.ReminderDataItem
-import dev.kelocen.reminders.utils.sendNotification
 import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 import kotlin.coroutines.CoroutineContext
 
 class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
@@ -19,49 +22,76 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO + coroutineJob
 
-    companion object {
-        private const val JOB_ID = 573
+    override fun onHandleWork(intent: Intent) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            Timber.e(getErrorMessage(this, geofencingEvent.errorCode))
+            return
+        }
+        if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            if (geofencingEvent.triggeringGeofences.isNullOrEmpty()) {
+                Timber.e(getErrorMessage(this, TRIGGERING_GEOFENCES_NULL_OR_EMPTY))
+            } else {
+                val triggeringGeofences: MutableList<Geofence> = geofencingEvent.triggeringGeofences
+                sendNotification(triggeringGeofences)
+            }
+        }
+    }
 
-        //        TODO: call this to start the JobIntentService to handle the geofencing transition events
+    /**
+     * Returns an error [String] using the given [Context] and [Int].
+     */
+    private fun getErrorMessage(context: Context, errorCode: Int): String {
+        val resources = context.resources
+        return when (errorCode) {
+            TRIGGERING_GEOFENCES_NULL_OR_EMPTY ->
+                resources.getString(R.string.geofences_null_or_empty)
+            GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE ->
+                resources.getString(R.string.geofence_not_available)
+            GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES ->
+                resources.getString(R.string.geofence_too_many_geofences)
+            GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS ->
+                resources.getString(R.string.geofence_too_many_pending_intents)
+            else -> resources.getString(R.string.geofence_unknown_error)
+        }
+    }
+
+    /**
+     * Retrieves a [ReminderDataItem] using the given [List] of [geofences][Geofence] and sends
+     * a location-based notification.
+     */
+    private fun sendNotification(geofences: MutableList<Geofence>) {
+        val remindersLocalRepository: ReminderDataSource by inject()
+        for (geofence in geofences) {
+            val requestId = geofence.requestId
+            CoroutineScope(coroutineContext).launch(SupervisorJob()) {
+                val result = remindersLocalRepository.getReminder(requestId)
+                if (result is Result.Success<ReminderDTO>) {
+                    val reminderDTO = result.data
+                    dev.kelocen.reminders.utils.sendNotification(
+                        this@GeofenceTransitionsJobIntentService, ReminderDataItem(
+                            reminderDTO.title,
+                            reminderDTO.description,
+                            reminderDTO.location,
+                            reminderDTO.latitude,
+                            reminderDTO.longitude,
+                            reminderDTO.id
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val TRIGGERING_GEOFENCES_NULL_OR_EMPTY = 0
+        private const val JOB_ID = 573
         fun enqueueWork(context: Context, intent: Intent) {
             enqueueWork(
                 context,
                 GeofenceTransitionsJobIntentService::class.java, JOB_ID,
                 intent
             )
-        }
-    }
-
-    override fun onHandleWork(intent: Intent) {
-        //TODO: handle the geofencing transition events and
-        // send a notification to the user when he enters the geofence area
-        //TODO call @sendNotification
-    }
-
-    //TODO: get the request id of the current geofence
-    private fun sendNotification(triggeringGeofences: List<Geofence>) {
-        val requestId = ""
-
-        //Get the local repository instance
-        val remindersLocalRepository: ReminderDataSource by inject()
-//        Interaction to the repository has to be through a coroutine scope
-        CoroutineScope(coroutineContext).launch(SupervisorJob()) {
-            //get the reminder with the request id
-            val result = remindersLocalRepository.getReminder(requestId)
-            if (result is Result.Success<ReminderDTO>) {
-                val reminderDTO = result.data
-                //send a notification to the user with the reminder details
-                sendNotification(
-                    this@GeofenceTransitionsJobIntentService, ReminderDataItem(
-                        reminderDTO.title,
-                        reminderDTO.description,
-                        reminderDTO.location,
-                        reminderDTO.latitude,
-                        reminderDTO.longitude,
-                        reminderDTO.id
-                    )
-                )
-            }
         }
     }
 }
